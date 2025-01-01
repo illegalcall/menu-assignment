10ebc5af7b212da3fa8a4f75eec453cd3fdd0c90
Dhruv Sharma <dhruvcoding67@gmail.com>
2025-01-01 20:47:43 +0530

    reverted to original main.ts

--- assignment-be/src/main.ts
+++ assignment-be/src/main.ts
@@ -1,48 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
+const port = process.env.PORT || 4000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-  // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Assginment API')
     .setDescription('This is the Assginment API description')
     .setVersion('1.0')
-    .build();
-    
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    .build()
 
-  // Global pipes
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     })
-  );
-
-  // CORS setup
+  )
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  })
 
-  // In a serverless environment (Vercel), we don't use app.listen(). Instead, we initialize the app.
-  if (process.env.VERCEL === '1') {
-    // If running in Vercel, don't use app.listen    
-    await app.init();
-  } else {
-    // If running locally, use app.listen to start the server.
-    await app.listen(4000);
-    console.log('NestJS app is running on http://localhost:4000');
-  }
+  await app.listen(port, '0.0.0.0'); 
 }
-
-// Vercel serverless function handler
-export default async (req: VercelRequest, res: VercelResponse) => {
-  await bootstrap();
-  res.status(200).send('Hello from NestJS on Vercel!');
-};
+bootstrap()
