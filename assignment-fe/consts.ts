c666fda4d3180cda5518878a1748657d66296036
Dhruv Sharma <dhruvcoding67@gmail.com>
2025-01-01 17:49:33 +0530

    added production flag for base url

--- assignment-fe/consts.ts
+++ assignment-fe/consts.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:4000'
+const isProduction = process.env.NODE_ENV === 'production';
+export const BASE_URL = isProduction ? process.env.NEXT_PUBLIC_PROD_URL : process.env.NEXT_PUBLIC_BASE_URL;
